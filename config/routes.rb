3c812638a9f3488de8ab584ec11cbcb25f5a11c7
Michael Palaiologos <Palaiologos1990@gmail.com>
2016-02-06 18:12:18 -0800

    Add projects view and section to index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get  'index'       => 'home#index'
   get  'about'       => 'home#about'
   get  'resume'      => 'home#resume'
-  get  'resume'      => 'home#projects'
+  get  'projects'    => 'home#projects'
   get  'contact'     => 'messages#new'
   post 'contact'     => 'messages#create'
   resources :messages, only: [:new, :create]
